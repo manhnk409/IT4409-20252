b80094a3330d6707fe8650d652373f0ec95ff72c
manhnk409 <manh.nd235374@sis.hust.edu.vn>
2026-05-27 16:55:47 +0700

    7

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+//test
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
 console.log("Connected to MongoDB");
