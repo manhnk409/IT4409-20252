80f66c3cf58c6d31004142df77d4466c73e688d3
manhnk409 <manh.nd235374@sis.hust.edu.vn>
2026-05-27 16:27:38 +0700

    3

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const express = require("express");
-const app = express();
+require("dotenv").config({quiet: true});
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+console.log("Connected to MongoDB");
+app.listen(3001, () => console.log("API running on port 3001"));
+})
+.catch((err) => {
+console.error("Connection Error:", err);
+process.exit(1);
+});
 app.get("/", async (req, res) => {
     res.status(200).send("Xin chào bạn");
 });
 
 // Start server
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT || 3001}`);
 });
 //test
